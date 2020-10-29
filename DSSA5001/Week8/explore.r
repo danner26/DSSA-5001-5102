0c849fe1aa2670847bba2fcd7a080c7a6e8430cc
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-10-29 14:30:55 -0400

    histograms and logarithm histograms working I think

--- DSSA5001/Week8/explore.r
+++ DSSA5001/Week8/explore.r
@@ -4,6 +4,11 @@
 # and calculates various statistical parameters and also creates 
 # a histogram graphic.
 library(tidyverse)
+library(readr)
+library(tibble)
+
+setwd("~/dssa_workspace/DSSA5001/Week8/")
+
 png("boxplot.png")
 d<-scan("streams_clean.csv")#"stdin", quiet=TRUE)
 cat("Minimum = ",min(d),"\n")
@@ -15,9 +20,14 @@ stem(d)
 boxplot(d)
 
 png("histogram.png")
-library(readr)
-streams_clean <- read_csv("dssa_workspace/DSSA5001/Week8/streams_clean.csv")
-ggplot(data = streams_clean, aes(x="Streams")) +
+streams_clean <- read_csv("data.csv")
+ggplot(data = streams_clean, aes(x = Streams)) +
   geom_histogram() +
-  labs(title = "Spotify Streams",
+  labs(title = "Spotify Streams - Histogram",
+       x = "Streams")
+
+png("loghistogram.png")
+ggplot(streams_clean, aes(x = Streams)) + geom_histogram() + 
+  scale_x_log10() +
+  labs(title = "Spotify Streams - Logarithm Histogram",
        x = "Streams")
