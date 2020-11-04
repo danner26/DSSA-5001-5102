d17c5ae879667fab57acef9808aa1c852bc4ac0f
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-11-04 17:36:48 +0000

    next test week 9

--- DSSA5001/Week9/plotter.py
+++ DSSA5001/Week9/plotter.py
@@ -22,7 +22,7 @@ print(min(diameter))
 #ax.scatter(feature1, feature2)
 #plt.savefig('example.png')
 
-ax.scatter(length, diameter, s=1)
+ax.scatter(length, diameter, s=1, c='coral')
 fig.suptitle('testing')
 plt.xlabel("Length");
 plt.ylabel("Diameter");
