febdd8c9b9ab95c39a423009797835e9d939c8d7
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-11-04 19:53:20 +0000

    next test week 9

--- DSSA5001/Week9/plotter.py
+++ DSSA5001/Week9/plotter.py
@@ -9,7 +9,7 @@ timestr = time.strftime("%H%M")
 df = pd.read_csv(sys.argv[1], delimiter=',', header=None, names=[ "sex", "length", "diameter", "height", "whole_weight", "shucked_weight", "viscera_weight", "shell_weight", "rings"])
 
 df1 = df[["length", "diameter"]]
-color_dict = {'diameter': '#FFD750', 'length': '#FF7F50'}
+color_dict = {'diameter': '#FF7F50', 'length': '#50FFD7'}
 
 fig, ax = plt.subplots(1,1)
 ax.scatter(df1["diameter"], df1["length"], s=10, c=[color_dict.get(x, '#333333') for x in df1.columns])
@@ -24,7 +24,7 @@ plt.savefig("png/abalone_data_2dimensions_" + timestr + ".png")
 
 
 #df2 = df[["length", "diameter", "whole_weight", "shell_weight"]]
-color_dict = {'length': '#FF7F50', 'diameter': '#FFD750', 'whole_weight': '#50FF7F', 'shell_weight': '#50FFD7'}
+color_dict = {'diameter': '#FF7F50', 'shell_weight': '#50FFD7', 'length': '#FFD750', 'whole_weight': '#50FF7F'}
 
 #fig2, ax2 = plt.subplots(1,1)
 #ax2.scatter(df[["diameter", "shell_weight"]], df[["length", "whole_weight"]], s=10, c=[color_dict.get(x, '#333333') for x in df2.columns], label=df2.)
