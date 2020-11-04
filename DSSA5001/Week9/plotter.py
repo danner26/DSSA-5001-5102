87df7c9aba15883d219f82cc68aaa7c37b11c3ac
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-11-04 18:44:44 +0000

    next test week 9

--- DSSA5001/Week9/plotter.py
+++ DSSA5001/Week9/plotter.py
@@ -7,7 +7,7 @@ file_name = sys.argv[1]
 df = pd.read_csv(sys.argv[1], names=[ "Sex", "Length", "Diameter", "Height", "Whole Weight", "Shucked Weight", "Viscera Weight", "Shell Weight", "Rings"])
 
 fig, ax = plt.subplots(1,1)
-ax.scatter(df["Length"], df["Diameter"], s=10, c='coral', zorder=1)
+ax.scatter(df["Length"], df["Diameter"], s=10, c='coral')
 fig.suptitle("Abalone Physical Statistics")
 #plt.rc('font', size=200)
 plt.xlabel("Length");
@@ -15,3 +15,15 @@ plt.ylabel("Diameter");
 
 timestr = time.strftime("%H%M")
 plt.savefig("png/abalone_data_2dimensions_" + timestr + ".png")
+
+df2 = df[["Length", "Diameter", "Whole Weight", "Shell Weight"]]
+
+fig, ax = plt.subplots(1,1)
+ax.scatter(df[["Diameter", "Shell Weight"]], df[["Length", "Whole Weight"]], s=10, c='coral')
+fig.suptitle("Abalone Physical Statistics")
+#plt.rc('font', size=200)
+plt.ylabel("Length & Whole Wright");
+plt.xlabel("Diameter & Shell Weight");
+
+timestr = time.strftime("%H%M")
+plt.savefig("png/abalone_data_4dimensions_" + timestr + ".png")
