b239ff301193590b9169b5a6d8cf6b3ca9544b9e
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-11-04 17:32:26 +0000

    next test week 9

--- DSSA5001/Week9/plotter.py
+++ DSSA5001/Week9/plotter.py
@@ -8,8 +8,6 @@ diameter = []
 with open(sys.argv[1], 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-#        if(len(row) != 9):
-#            print("abnormal")
         length.append(float(row[1].strip('"')))
         diameter.append(float(row[2].strip('"')))
 
@@ -22,12 +20,13 @@ print(min(diameter))
 #feature1 = [1,2,3,4,5,6]
 #feature2 = [3,4,5,6,5,3]
 #ax.scatter(feature1, feature2)
-#plt.xlim(0,1)
-#plt.ylim(0,1)
+#plt.savefig('example.png')
+
 ax.scatter(length, diameter)
 fig.suptitle('testing')
-ax.set_xlabel('Length')
-ax.set_ylabel('Diameter')
+plt.xlabel("Length");
+plt.ylabel("Diameter");
+#ax.set_xlabel('Length')
+#ax.set_ylabel('Diameter')
 
-#plt.savefig('example.png')
 plt.savefig('figure.png')
