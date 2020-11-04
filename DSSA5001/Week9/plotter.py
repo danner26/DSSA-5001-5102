606459f9340465d6bb5e56b5a71330f358de5923
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-11-04 19:34:33 +0000

    next test week 9

--- DSSA5001/Week9/plotter.py
+++ DSSA5001/Week9/plotter.py
@@ -24,7 +24,7 @@ df2 = df[["length", "diameter", "whole_weight", "shell_weight"]]
 color_dict = {'length': '#FF7F50', 'diameter': '#FFD750', 'whole_weight': '#50FF7F', 'shell_weight': '#50FFD7'}
 
 fig2, ax2 = plt.subplots(1,1)
-ax2.scatter(df[["diameter", "shell_weight"]], df[["length", "whole_weight"]], s=10, c=[color_dict.get(x, '#333333') for x in df2.columns])
+ax2.scatter(df[["diameter", "shell_weight"]], df[["length", "whole_weight"]], s=10, c=[color_dict.get(x, '#333333') for x in df2.columns], label=color)
 fig2.suptitle("Abalone Physical Statistics")
 ax2.legend()
 plt.ylabel("Length & Whole Wright");
