57c4874456d071c1b46f01d4487af9dc2fdf14e4
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-11-04 19:45:03 +0000

    next test week 9

--- DSSA5001/Week9/plotter.py
+++ DSSA5001/Week9/plotter.py
@@ -20,13 +20,15 @@ plt.savefig("png/abalone_data_2dimensions_" + timestr + ".png")
 
 
 
-df2 = df[["length", "diameter", "whole_weight", "shell_weight"]]
+#df2 = df[["length", "diameter", "whole_weight", "shell_weight"]]
 color_dict = {'length': '#FF7F50', 'diameter': '#FFD750', 'whole_weight': '#50FF7F', 'shell_weight': '#50FFD7'}
 
-fig2, ax2 = plt.subplots(1,1)
-ax2.scatter(df[["diameter", "shell_weight"]], df[["length", "whole_weight"]], s=10, c=[color_dict.get(x, '#333333') for x in df2.columns], label=df2)
-fig2.suptitle("Abalone Physical Statistics")
-ax2.legend()
+#fig2, ax2 = plt.subplots(1,1)
+#ax2.scatter(df[["diameter", "shell_weight"]], df[["length", "whole_weight"]], s=10, c=[color_dict.get(x, '#333333') for x in df2.columns], label=df2.)
+#fig2.suptitle("Abalone Physical Statistics")
+ax.scatter(df["whole_weight"], df["shell_weight"], c=[color_dict.get(x, '#333333') for x in df.columns])
+
+#ax2.legend()
 plt.ylabel("Length & Whole Wright");
 plt.xlabel("Diameter & Shell Weight");
 
