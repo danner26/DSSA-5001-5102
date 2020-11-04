247bb6a30e118e9fb2fc526182e0cf775beeeda1
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-11-04 20:12:31 +0000

    next test week 9

--- DSSA5001/Week9/plotter.py
+++ DSSA5001/Week9/plotter.py
@@ -8,11 +8,11 @@ timestr = time.strftime("%H%M")
 
 df = pd.read_csv(sys.argv[1], delimiter=',', header=None, names=[ "sex", "length", "diameter", "height", "whole_weight", "shucked_weight", "viscera_weight", "shell_weight", "rings"])
 
-df1 = df[["length", "diameter"]]
-color_dict = {'length': '#50FFD7', 'diameter': '#FF7F50'}
+#df1 = df[["length", "diameter"]]
+#color_dict = {'length': '#50FFD7', 'diameter': '#FF7F50'}
 
 fig, ax = plt.subplots(1,1)
-ax.scatter(df1["length"], df1["diameter"], s=10, c=[color_dict.get(x, '#333333') for x in df1.columns], label=[color_dict.get(x, "x") for x in df1.columns])
+ax.scatter(df["length"], df["diameter"], s=10, c='#FF7F50') #[color_dict.get(x, '#333333') for x in df1.columns], label=[color_dict.get(x, "x") for x in df1.columns])
 fig.suptitle("Abalone Physical Statistics")
 plt.xlabel("Length");
 plt.ylabel("Diameter");
@@ -25,15 +25,17 @@ plt.savefig("png/abalone_data_2dimensions_" + timestr + ".png")
 
 
 #df2 = df[["length", "diameter", "whole_weight", "shell_weight"]]
-color_dict = {'diameter': '#FF7F50', 'shell_weight': '#50FFD7', 'length': '#FFD750', 'whole_weight': '#50FF7F'}
+#color_dict = {'diameter': '#FF7F50', 'shell_weight': '#50FFD7', 'length': '#FFD750', 'whole_weight': '#50FF7F'}
 
 #fig2, ax2 = plt.subplots(1,1)
 #ax2.scatter(df[["diameter", "shell_weight"]], df[["length", "whole_weight"]], s=10, c=[color_dict.get(x, '#333333') for x in df2.columns], label=df2.)
 #fig2.suptitle("Abalone Physical Statistics")
-ax.scatter(df["whole_weight"], df["shell_weight"], c=[color_dict.get(x, '#333333') for x in df.columns])
+#ax.scatter(df["whole_weight"], df["shell_weight"], c=[color_dict.get(x, '#333333') for x in df.columns])
+ax.scatter(df["length"], df["whole_weight"], c="#50FF7F")
+ax.scatter(df["length"], df["shell_weight"], c="#50FFD7")
+ax.legend()
 
-#ax2.legend()
-plt.ylabel("Length & Whole Wright");
-plt.xlabel("Diameter & Shell Weight");
+#plt.ylabel("Length & Whole Wright");
+#plt.xlabel("Diameter & Shell Weight");
 
 plt.savefig("png/abalone_data_4dimensions_" + timestr + ".png")
