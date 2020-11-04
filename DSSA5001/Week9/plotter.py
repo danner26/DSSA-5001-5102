13cf3078ea264f122dc0f18379e33a49c6193db8
Daniel W. Anner <daniel.anner@danstechsupport.com>
2020-11-04 16:51:57 +0000

    week 9 task 3

--- DSSA5001/Week9/plotter.py
+++ DSSA5001/Week9/plotter.py
@@ -1,12 +1,24 @@
-import matplotlib.pyplot as plt
+import matplotlib, csv, sys
+matplotlib.use("Agg")
+from matplotlib import pyplot as plt
+
+length = []
+diameter = []
+
+with open(sys.argv[1], 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+#        if(len(row) != 9):
+#            print("abnormal")
+        length.append(row[1])
+        diameter.append(row[2])
 
 fig, ax = plt.subplots(1,1)
 
-feature1 = [1,2,3,4,5,6]
-feature2 = [3,4,5,6,5,3]
-ax.scatter(feature1,feature2)
-ax.set_ylabel('feature 1')
-ax.set_ylabel('feature 2')
+#feature1 = [1,2,3,4,5,6]
+#feature2 = [3,4,5,6,5,3]
+ax.scatter(length, diameter)
+ax.set_ylabel('Length')
+ax.set_ylabel('Diameter')
 
 plt.savefig('figure.png')
-	
