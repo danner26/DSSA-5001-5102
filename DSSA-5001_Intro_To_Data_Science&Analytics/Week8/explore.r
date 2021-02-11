67e09ab737d67fa8cd286b0cc1aa13c25a056295
Daniel Anner <daniel.anner@danstechsupport.com>
2021-02-11 16:55:14 -0500

    Merge data to new folder structure

--- DSSA-5001_Intro_To_Data_Science&Analytics/Week8/explore.r
+++ DSSA-5001_Intro_To_Data_Science&Analytics/Week8/explore.r
@@ -1,33 +1,3 @@
-#! /usr/bin/env Rscript
-# This R script reads a column of data points from the standard
-# input using $ ./explore.r < filename.dat
-# and calculates various statistical parameters and also creates 
-# a histogram graphic.
-library(tidyverse)
-library(readr)
-library(tibble)
-
-setwd("~/dssa_workspace/DSSA5001/Week8/")
-
-png("boxplot.png")
-d<-scan("streams_clean.csv")#"stdin", quiet=TRUE)
-cat("Minimum = ",min(d),"\n")
-cat("Maximum = ",max(d),"\n")
-cat("Median  = ",median(d),"\n")
-cat("Mean    = ",mean(d),"\n")
-summary(d)
-stem(d)
-boxplot(d)
-
-png("histogram.png")
-streams_clean <- read_csv("data.csv")
-ggplot(data = streams_clean, aes(x = Streams)) +
-  geom_histogram() +
-  labs(title = "Spotify Streams - Histogram",
-       x = "Streams")
-
-png("loghistogram.png")
-ggplot(streams_clean, aes(x = Streams)) + geom_histogram() + 
-  scale_x_log10() +
-  labs(title = "Spotify Streams - Logarithm Histogram",
-       x = "Streams")
+version https://git-lfs.github.com/spec/v1
+oid sha256:5cc518c284b8f77cb55a9f7d5f678c90dc7a27ab01dfb95859a491de4ccfa44d
+size 953
